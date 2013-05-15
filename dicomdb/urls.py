d68f59201a7c14b237c41c4d65109aa844899b50
Eric Bower <neurosnap@gmail.com>
2013-05-15 08:46:30 -0400

    url redirect

--- dicomdb/urls.py
+++ dicomdb/urls.py
@@ -1,22 +1,37 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import settings
+#import settings
 from django.contrib import admin
+import socket
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dicomdb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    (r'^dicoms/(?P<path>.*)$', 'django.views.static.serve',
-	    {'document_root': settings.MEDIA_ROOT}),
-    url(r'^$', include('main.urls')),
-    url(r'^main/', include('main.urls')),
-    url(r'^users/', include('users.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^upload/', include('upload.urls')),
-    url(r'^browse/', include('browse.urls')),
-    url(r'^dcmview/', include('dcmview.urls')),
-)
+from main import views as main_views
+
+host = socket.gethostname()
+
+#see if it's production
+if host == "dcmdb.org" or host == "72.240.79.150":
+
+    urlpatterns = patterns('',
+        url(r'^$', main_views.under_construction, name = "under_construction"),
+    )
+
+else:
+
+    urlpatterns = patterns('',
+        # Examples:
+        # url(r'^$', 'dicomdb.views.home', name='home'),
+        # url(r'^blog/', include('blog.urls')),
+        #(r'^dicoms/(?P<path>.*)$', 'django.views.static.serve',
+    	#    {'document_root': settings.MEDIA_ROOT}),
+        url(r'^$', include('main.urls')),
+        url(r'^main/', include('main.urls')),
+        url(r'^users/', include('users.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^upload/', include('upload.urls')),
+        url(r'^browse/', include('browse.urls')),
+        url(r'^dcmview/', include('dcmview.urls')),
+    )
 
 urlpatterns += staticfiles_urlpatterns()
--- main/views.py
+++ main/views.py
@@ -16,3 +16,9 @@ def about(request):
 	context = {}
 	
 	return render_to_response('about.html', context, context_instance = RequestContext(request))
+
+def under_construction(request):
+
+	context = {}
+	
+	return render_to_response('under_construction.html', context, context_instance = RequestContext(request))
