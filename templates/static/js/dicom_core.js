7e816749cf94f7a7e9603eb478c557b4cdc938ad
Eric Bower <neurosnap@gmail.com>
2013-11-02 13:28:08 -0400

    added external links go to new tab feature

--- templates/static/js/dicom_core.js
+++ templates/static/js/dicom_core.js
@@ -1,5 +1,16 @@
+$.expr[':'].external = function(obj) {
+
+    return !obj.href.match(/^mailto\:/)
+           && (obj.hostname != location.hostname)
+           && !obj.href.match(/^javascript\:/)
+           && !obj.href.match(/^$/);
+
+};
+
 $(function() {
 	
+    $('a:external').attr('target', '_blank');
+
 	var csrftoken = getCookie('csrftoken');
     
 	$.ajaxSetup({
